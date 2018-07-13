3879d83b8529edcbe0e4c68a2b22ebe7eb0b7d8a
Kimserey Lam <kimserey.lam@groomgy.com>
2018-07-13 10:29:37 +0100

    more example

--- Example/Startup.cs
+++ Example/Startup.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using IdentityModel.AspNetCore.OAuth2Introspection;
+using IdentityServer4.AccessTokenValidation;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace Example
 {
@@ -14,7 +17,53 @@ namespace Example
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSignalR();
+
             services.AddMvc();
+
+            services
+                .AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+                    options.ApiName = "my-api";
+                    options.NameClaimType = "sub";
+                    options.TokenRetriever = new Func<HttpRequest, string>(req =>
+                    {
+                        var fromHeader = TokenRetrieval.FromAuthorizationHeader();
+                        var fromQuery = TokenRetrieval.FromQueryString();
+                        return fromHeader(req) ?? fromQuery(req);
+                    });
+                });
+
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryClients(new[] {
+                    new Client {
+                        ClientId = "signalr-app",
+                        ClientName = "signalr-app",
+                        ClientSecrets = { new Secret("secret".Sha256()) },
+                        AllowedScopes = { "signalr" },
+                        AllowedGrantTypes = GrantTypes.ResourceOwnerPassword
+                    }
+                })
+                .AddInMemoryApiResources(new[] {
+                    new ApiResource("signalr", "SignalR Test API")
+                })
+                .AddInMemoryIdentityResources(new List<IdentityResource> {
+                    new IdentityResources.OpenId(),
+                    new IdentityResources.Profile(),
+                    new IdentityResources.Email()
+                })
+                .AddInMemoryPersistedGrants()
+                .AddTestUsers(new List<TestUser>{
+                    new TestUser {
+                        SubjectId = "alice",
+                        Username = "alice",
+                        Password = "password",
+                        Claims = new[] { new Claim("role", "admin") }
+                    }
+                });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -26,9 +75,13 @@ namespace Example
 
             app.UseStaticFiles();
 
+            app.UseAuthentication();
+
             app.UseSignalR(hubRouteBuilder => {
                 hubRouteBuilder.MapHub<ChatHub>("/chathub");
             });
+
+            app.UseIdentityServer();
 
             app.UseMvc();
         }
